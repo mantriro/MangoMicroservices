0cca99bfd2298e57fd46b4de17b23d09f3116285
mantriro <mrohit58@gmail.com>
2024-08-14 00:59:52 -0400

    Mango.Web added product index and front page, disabled auth in productapi get for this as well
    ShoppingCartAPI added, controller, appdbcontext, dtos, models, mapping and auth extensions, appsettings

---
+++ Mango.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI
+{
+	public class MappingConfig
+	{
+		public static MapperConfiguration RegisterMaps()
+		{
+			var mappingConfig= new MapperConfiguration(config =>
+			{
+				config.CreateMap<CartDetailsDto, CartDetails>().ReverseMap();
+				config.CreateMap<CartHeaderDto, CartHeader>().ReverseMap();
+//                config.CreateMap<CartDetails, CartDetailsDto>();
+//                config.CreateMap<CartHeader, CartHeaderDto>();
+
+            });
+			return mappingConfig;
+		}
+	}
+}
